3f1833ff9eb2db49530d9b5e7d4551250c87a0e8
Strelya <serdjiostrel@gmail.com>
2018-12-29 15:17:37 +0200

    routing

--- index.php
+++ index.php
@@ -2,54 +2,53 @@
 
 include 'app/connect.php';
 
-    $arr = require 'app/routes.php';
+    /*$arr = require 'app/routes.php';
     foreach ($arr as $key => $val) {
         $this->add($key, $val);
         var_dump($this);
-    }
-    
+    }*/
 
-/*if (isset($_GET["page"])) {
-	$start = ($_GET["page"]*8)-8;
-	$query = "SELECT * FROM posts LIMIT ".$start.", 8";
-	$title = $h1 = "Отзывы - Страница ".$_GET["page"];
-}
-elseif (isset($_GET["post"])) {
-	$query = "SELECT * FROM posts WHERE id = ".$_GET["post"];
-	$title = $h1 = "Отзыв ".$_GET["post"];
-}
-else
-{
-	$query = "SELECT * FROM posts LIMIT 8";
-	$title = $h1 = "Отзывы";
-}
-
-
-$request = $_SERVER['REDIRECT_URL'];
 include 'views/head.php';
 include 'views/nav.php';
 
-switch ($request) {
-    case '/' :
-        include __DIR__ . '/views/main.php';
-        break;
-    case '' :
-        include __DIR__ . '/views/main.php';
-        break;
+if(isset($_SERVER['REDIRECT_URL']))
+{
+switch ($_SERVER['REDIRECT_URL']) {
     case '/about' :
-		$title = $h1 = "О нас";
+        $title = $h1 = "О нас";
         include __DIR__ . '/views/about.php';
-		
+        
         break;
     default:
-		$title = $h1 = "404";
-		require __DIR__ . '/views/404.php';
-		//header('HTTP/1.0 404 Not Found');
+        $title = $h1 = "404";
+        require __DIR__ . '/views/404.php';
+        //header('HTTP/1.0 404 Not Found');
         break;
-}*/
-/*$query_pages = $sql->query("SELECT COUNT(*) FROM posts");
-$count = $query_pages->fetchColumn();
-$pages = round($count/8, 0, PHP_ROUND_HALF_UP);*/
+}
+}
+elseif (isset($_GET["page"])) {
+    $start = ($_GET["page"]*8)-8;
+    $query = "SELECT * FROM posts LIMIT ".$start.", 8";
+    $title = $h1 = "Отзывы - Страница ".$_GET["page"];
+    $query_pages = $sql->query("SELECT COUNT(*) FROM posts");
+    $count = $query_pages->fetchColumn();
+    $pages = round($count/8, 0, PHP_ROUND_HALF_UP);
+    include __DIR__ . '/views/main.php';
+}
+elseif (isset($_GET["post"])) {
+    $query = "SELECT * FROM posts WHERE id = ".$_GET["post"];
+    $title = $h1 = "Идентификатор ".$_GET["post"];
+    
+    include __DIR__ . '/views/single_post.php';
+}
+else
+{
+    $query = "SELECT * FROM posts LIMIT 8";
+    $title = $h1 = "Отзывы";
+    $query_pages = $sql->query("SELECT COUNT(*) FROM posts");
+    $count = $query_pages->fetchColumn();
+    $pages = round($count/8, 0, PHP_ROUND_HALF_UP);
+    include __DIR__ . '/views/main.php';
+}
 
-include 'views/paginate.php';
 include 'views/footer.php';
--- views/single_post.php
+++ views/single_post.php
@@ -12,21 +12,16 @@
     foreach ($sql->query($query) as $row) {
     ?>
 		<div class="col-lg-3 col-md-4 col-xs-6">
-          <a href="/?post=<?=$row["id"]?>" class="d-block mb-4 h-100">
             <img class="img-fluid img-thumbnail" src="http://placehold.it/320x240" alt="">
 		  <div class="caption">
             <h3><?=$row["name"]?></h3>
             <p><?=$row["text"]?></p>
           </div>          
-		  </a>
-
-        </div>
+    </div>
 	<?php		
     }
-
 ?>
     
       </div>
     </div>
     <!-- /.container -->
-  <?php  include 'views/paginate.php'; ?>    
